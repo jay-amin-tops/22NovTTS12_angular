3339f04a32a89c694815f12a73b0672fa7001d8b
jay-amin-tops <jay.amin.tops@gmail.com>
2022-11-26 13:00:13 +0530

    TS basic

---
+++ TS/11array.js
@@ -0,0 +1,14 @@
+var array_name = ['val1', 'val2', 'valn', 123, true];
+console.log(array_name);
+console.log(array_name[0]);
+var alphas;
+alphas = ["1", "2", "3", "4"];
+console.log(alphas[0]);
+console.log(alphas[1]);
+var arr = ["orange", "mango", "banana", "sugar", "tea"];
+var removed = arr.splice(2, 0, "water");
+// console.log("After adding 1: " + arr );  
+// console.log("removed is: " + removed); 
+removed = arr.splice(3, 1);
+console.log("After removing 1: " + arr);
+console.log("removed is: " + removed);
---
+++ TS/14Interface.js
@@ -0,0 +1,6 @@
+var customer = {
+    firstName: "Tom",
+    lastName: "Hanks",
+    sayHi: function () { return "Hi there"; }
+};
+console.log(customer);
