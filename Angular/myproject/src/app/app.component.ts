d2691420a8b85ad35663496686d7e8281caf118c
jay-amin-tops <jay.amin.tops@gmail.com>
2022-12-06 12:50:54 +0530

    Angular bootstrap

---
+++ Angular/myproject/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+// templateUrl: './app.component.html',
+// template: './app.component.html',
+// template: '<h2>Something</h2>
+//   form
+// ',
+// template: `<h2>Something</h2>
+//   <form>
+//     <input>
+//   </form>
+// `,
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'myproject';
+}
